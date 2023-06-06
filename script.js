e1457d783a2086f00390e72f57670bc99107c883
dotmatrixIO <luke.miers@tutanota.com>
2023-06-06 14:41:24 +0800

    Update completed exercise

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+playerscore = 0;
+computerscore = 0
+
+/* Add div to keep track of running total */
+const results = document.querySelector('#results');
+const content = document.createElement('div');
+content.classList.add('content');
+
+
+/* Allow buttons to run a new game */
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+button.addEventListener('click', () => {
+
+    game();
+
+  });
+});
+
+
 /* function to return either rock, paper or scissors */
 function getComputerChoice()
 {
@@ -21,6 +41,7 @@ function getComputerChoice()
 function playRound(playerSelection, computerSelection)
 {
 
+
     /* call function for computer choice */
     computerSelection = getComputerChoice()
 
@@ -30,53 +51,119 @@ function playRound(playerSelection, computerSelection)
 
     /* conditions for the game's result */  
     if (playerSelection == 'rock' & computerSelection == 'Rock')
-        result = "Draw! Both players choose Rock!"
+    {
+        content.textContent = 'Draw! Both players chooose Rock!';
+        results.appendChild(content);
+        
+    }
         
      
-    if (playerSelection == 'paper' & computerSelection == 'Rock')
-        result = "You win! Paper beats Rock!"
-  
+    else if (playerSelection == 'paper' & computerSelection == 'Rock')
+    {
+        content.textContent = 'You win! Paper beats Rock';
+        results.appendChild(content);
+        playerscore ++;
+        
+        /* update score */
+        document.getElementById("p1").innerHTML = ("Player score: " + playerscore);
+    }
     
-    if (playerSelection == 'scissors' & computerSelection == 'Rock')
-        result = "You lose! Rock beats Scissors!"
+    else if (playerSelection == 'scissors' & computerSelection == 'Rock')
+    {
+        content.textContent = 'You lose! Rock beats Scissors!';
+        results.appendChild(content);
+        computerscore ++;
+
+        /* update score */
+        document.getElementById("p2").innerHTML = ("Computer score: " + computerscore);
+    }
 
 
+    else if (playerSelection == 'rock' & computerSelection == 'Paper')
+    {
+        content.textContent = 'You lose! Paper beats rock!';
+        results.appendChild(content);
+        computerscore ++;
 
-    if (playerSelection == 'rock' & computerSelection == 'Paper')
-        result = "You lose! Paper beats Rock!"
+        /* update score */
+        document.getElementById("p2").innerHTML = ("Computer score: " + computerscore);
+    }
         
     
-    if (playerSelection == 'paper' & computerSelection == 'Paper')
-        result = "Draw! Both players choose paper!"
+    else if (playerSelection == 'paper' & computerSelection == 'Paper')
+    {
+        content.textContent = "Draw! Both players choose paper!";
+        results.appendChild(content);
+    }
 
     
-    if (playerSelection == 'scissors' & computerSelection == 'Paper')
-        result = "You win! Scissors beats Paper!"
+    else if (playerSelection == 'scissors' & computerSelection == 'Paper')
+    {
+        content.textContent = "You win! Scissors beats Paper!";
+        results.appendChild(content);
+        playerscore ++;
 
-
+        /* update score */
+        document.getElementById("p1").innerHTML = ("Player score: " + playerscore);
+    }
 
     
-    if (playerSelection == 'rock' & computerSelection == 'Scissors')
-        result = "You win! Rock beats Scissors!"
+    else if (playerSelection == 'rock' & computerSelection == 'Scissors')
+    {
+        content.textContent = "You win! Rock beats Scissors!";
+        results.appendChild(content);
+        playerscore ++;
+
+        /* update score */
+        document.getElementById("p1").innerHTML = ("Player score: " + playerscore);
+    }
 
     
-    if (playerSelection == 'paper' & computerSelection == 'Scissors')
-        result = "You lose! Scissors beats Paper!"
+    else if (playerSelection == 'paper' & computerSelection == 'Scissors')
+    {
+        content.textContent = "You lose! Scissors beats Paper!";
+        results.appendChild(content);
+        computerscore ++;
+
+        /* update score */
+        document.getElementById("p2").innerHTML = ("Computer score: " + computerscore);
+    }
 
 
-    if (playerSelection == 'scissors' & computerSelection == 'Scissors')
-        result = "Draw! Both players choose paper!"
+    else if (playerSelection == 'scissors' & computerSelection == 'Scissors')
+    {
+        content.textContent = "Draw! Both players choose paper!";
+        results.appendChild(content);
+    }
+
 
 }
 
 /* function to run the game - call this in the while loop in index.html */ 
 function game()
 {
+  
+    let ask = prompt("Type Rock, Paper or Scissors")
+    playerInput = ask.toString()
 
     /* allow for case-insensitive player input */
     playerChoice = playerInput.toLowerCase()
 
     playRound(playerChoice, getComputerChoice)
-    console.log(result)
+
+
+    if (playerscore == 5 || computerscore == 5)
+    {
+        alert("You win!")
+    }
+
+    else if (computerscore == 5)
+    {
+        alert("You lose :(")
+    }
 
 }
+
+
+
+
